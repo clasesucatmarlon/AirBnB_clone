9a274032aeb183aa61737aae21ae4b811fdf906e
Camilo tobon <1611@holbertonschool.com>
2020-06-26 19:14:38 -0500

    adding show command

--- console.py
+++ console.py
@@ -1,4 +1,5 @@
 import cmd
+from json import loads, dumps
 from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
@@ -19,6 +20,33 @@ class HBNBCommand(cmd.Cmd):
             except NameError:
                 print('** class doesnt exist **')
                 pass
+    
+    def do_show(self, args):
+        tmp_dictionary = {}
+        parsed = args.split(' ')
+        if parsed == ['']:
+            print('** class name missing **')
+        elif len(parsed) == 1:
+            print('** instance id missing *')
+        if len(parsed) == 2:
+            instancia, instance_id = parsed[0], parsed[1]
+            with open('file.json', 'r') as jsonfile:
+                tmp_dictionary = loads(jsonfile.read())
+            key = instancia + '.' + instance_id
+            try:
+                new_ins = eval(instancia)
+            except:
+                print('** class doesnt exist **')
+                return 0
+            try:
+                new_ins = eval(instancia)(**tmp_dictionary[key])
+                print(new_ins)
+            except:
+                print('** no instance found **')
+                return 0
+        else:
+            pass
+
 
     def do_quit(self, args):
         """
