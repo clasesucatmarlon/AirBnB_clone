06e0ceddff2f2e47b50d19f72854238c083108a2
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-06-28 14:10:51 -0500

    added class state

---
+++ models/state.py
@@ -0,0 +1,31 @@
+from models.base_model import BaseModel, time_conversor
+from datetime import datetime
+
+
+class State(BaseModel):
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        BaseModel.__init__(self, *args, **kwargs)
+
+    def __str__(self):
+        self.__dict__.update({
+            "created_at": time_conversor(self.created_at),
+            "updated_at": time_conversor(self.updated_at),
+            "name" : self.name,
+            "__class__" : self.__class__.__name__
+        })
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    
+    def to_dict(self):
+        if type(self.created_at) in [str]:
+            self.created_at = time_conversor(self.created_at)
+        if type(self.updated_at) in [str]:
+            self.updated_at = time_conversor(self.updated_at)
+        self.__dict__.update({
+            "name" : self.name,
+            "created_at" : self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f%z'),
+            "updated_at" : self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f%z'),
+            "__class__" : self.__class__.__name__
+        })
+        return self.__dict__
