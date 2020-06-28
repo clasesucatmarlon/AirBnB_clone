785e2f5945a647edfecda8b6e4945c44c4b0533b
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-06-28 14:28:11 -0500

    added class Place and Review to list

--- models/__init__.py
+++ models/__init__.py
@@ -5,4 +5,4 @@ from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
 
-classes = ["BaseModel", "User", "State", "City", "Amenity"]
+classes = ["BaseModel", "User", "State", "City", "Amenity", "Place", "Review"]
