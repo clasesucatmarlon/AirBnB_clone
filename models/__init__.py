3b418d534392d0d6cd513bc655860feec35be089
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-06-28 14:07:45 -0500

    added class  state, city and amenity to list

--- models/__init__.py
+++ models/__init__.py
@@ -5,4 +5,4 @@ from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
 
-classes = ["BaseModel", "User"]
+classes = ["BaseModel", "User", "State", "City", "Amenity"]
