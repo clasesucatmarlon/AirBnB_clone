1d735d0f1092c21def2ea84a7b996b84f981105c
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-06-29 20:29:35 -0500

    test amenity

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import unittest
+from models.amenity import Amenity
+
+
+class TestAmenity(unittest.TestCase):
+    """ test for amenity """
+    def setUp(self):
+        """ standard setUp()
+        """
+        self.model = Amenity()
+
+    def test_public_attr(self):
+        """ if public attribute exists and if equal to empty string-
+        name("")
+        """
+        self.assertTrue(hasattr(self.model, "name"))
+        self.assertEqual(self.model.name, "")
+
+    def test_string(self):
+        """
+        assertEqual input for each attr
+        """
+        self.model.name = "Wifi"
+        self.assertEqual(self.model.name, "Wifi")
+
+if __name__ == '__main__':
+    unittest.main()
