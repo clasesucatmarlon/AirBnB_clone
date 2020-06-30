159e68500fcb8b88283c9853e3ae4a3aeb8f8dc0
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-06-29 19:52:29 -0500

    updated test base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,37 +1,62 @@
 #!/usr/bin/python3
+""" Contains unit tests for class BaseModel
+"""
 import unittest
+import io
+import sys
 from datetime import datetime
-
 from models.base_model import BaseModel
+import models
 
 
 class TestBaseModel(unittest.TestCase):
-    """ test model: base_model  """
+    """test for class BaseModel and its methods
+    """
     def setUp(self):
-        """ standard setUp() """
-        self.model = BaseModel()
+        """ Set up method
+        """
+        self.base1 = BaseModel()
+        self.base2 = BaseModel()
 
-    def test_name_number(self):
-        """ test name and number """
-        self.model.name = "Holberton"
-        self.model.my_number = 89
-        self.assertEqual(self.model.name, "Holberton")
-        self.assertEqual(self.model.my_number, 89)
-    
-    def test_public_attr(self):
-        # id (uuid), created_at (datetime), updated_at (datetime)
-        self.assertEqual(self.model.__class__.__name__, "BaseModel")
-        self.assertFalse(hasattr(self.model, "name"))
-        self.assertFalse(hasattr(self.model, "my_number"))
-        self.assertTrue(hasattr(self.model, "id"))
-        self.assertTrue(hasattr(self.model, "created_at"))
-        self.assertFalse(hasattr(self.model, "updated_at"))
-    
+    def tearDown(self):
+        """ Tear down method
+        """
+        pass
+
+    def test_uuid(self):
+        self.assertNotEqual(self.base1.id, self.base2.id)
+        self.assertTrue(hasattr(self.base1, "id"))
+        self.assertEqual(type(self.base1.id), str)
+        self.assertEqual(type(self.base2.id), str)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.base1, BaseModel))
+        self.assertTrue(isinstance(self.base2, BaseModel))
+
+    def test_type(self):
+        self.assertEqual(type(self.base1), BaseModel)
+        self.assertEqual(type(self.base2), BaseModel)
+
     def test_save(self):
-        """ save  """
-        self.model.save()
+        time = self.base1.updated_at
+        self.base1.save()
+        self.assertFalse(time == self.base1.updated_at)
 
-    def test_to_dict(self):
-        """ __dict__ (instance + class name in the key __class__) """
-        self.dictmodel = self.model.to_dict()
-        self.assertEqual(self.dictmodel, self.model.__dict__)
+    def test_update(self):
+        self.base1.name = "Holberton"
+        self.assertTrue(hasattr(self.base1, "name"))
+
+    def test_init_with_kwargs(self):
+        self.base1.name = "Holberton"
+        model_json = self.base1.to_dict()
+        new_model = BaseModel(**model_json)
+        self.assertDictEqual(model_json, new_model.to_dict())
+        self.assertIn("name", new_model.to_dict())
+        self.assertIsNot(self.base1, new_model)
+
+    def test_storage(self):
+        obj_dict = models.storage.all()
+        self.assertTrue(obj_dict)
+
+if __name__ == '__main__':
+    unittest.main()
