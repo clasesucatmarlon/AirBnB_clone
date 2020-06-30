452f681f206d37af1c865a12932c8460431f9cdc
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-06-29 21:16:53 -0500

    pep8

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -29,5 +29,6 @@ class TestCity(unittest.TestCase):
         self.assertEqual(self.model.state_id,
                          "7d78d8de-a37f-4edd-9443-1578032a1eea")
 
+
 if __name__ == '__main__':
     unittest.main()
