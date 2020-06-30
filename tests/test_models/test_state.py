0914ee16c5e9ec937fe349385ffef3f48f362c0a
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-06-30 10:32:35 -0500

    added test state

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import unittest
+from datetime import datetime
+from models.state import State
+
+
+class TestState(unittest.TestCase):
+    """ test for state
+    """
+
+    def setUp(self):
+        """ standard setUp()
+        """
+        self.model = State()
+
+    def test_public_attr(self):
+        """ if public attribute exists and if equal to empty string
+        """
+        self.assertTrue(hasattr(self.model, "name"))
+        self.assertEqual(self.model.name, "")
+
+    def test_string(self):
+        """ input for each attr
+        """
+        self.model.name = "California"
+        self.assertEqual(self.model.name, "California")
+
+
+if __name__ == '__main__':
+    unittest.main()
