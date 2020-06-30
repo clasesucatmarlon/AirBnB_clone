52436e37a066792a5799824ed9b1182258b023d7
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-06-29 20:31:18 -0500

    added comment base model

--- models/base_model.py
+++ models/base_model.py
@@ -6,61 +6,61 @@ import models
 
 
 def time_conversor(obj):
-    """ Define time conversor
-    """
-    if type(obj) in [datetime]:
-        obj = obj.strftime('%Y-%m-%dT%H:%M:%S.%f')
-    return datetime.strptime(obj, "%Y-%m-%dT%H:%M:%S.%f")
+	""" Define time conversor
+	"""
+	if type(obj) in [datetime]:
+		obj = obj.strftime('%Y-%m-%dT%H:%M:%S.%f')
+	return datetime.strptime(obj, "%Y-%m-%dT%H:%M:%S.%f")
 
 class BaseModel:
-    """ Class Base Model
-    """
-    def __init__(self, *args, **kwargs):
-        """ Initialize method init
-        """
-        if kwargs:
-            self.created_at = time_conversor(kwargs["created_at"])
-            self.updated_at = time_conversor(kwargs["updated_at"])
-            for k, v in kwargs.items():
-                if k == '__class__':
-                    continue
-                else:
-                    setattr(self, k, v)
-        else:
-            self.id = str(uuid4())
-            self.created_at = datetime.today().isoformat()
-            self.updated_at = datetime.today().isoformat()
-            models.storage.new(self)
+	""" Class Base Model
+	"""
+	def __init__(self, *args, **kwargs):
+		""" Initialize method init
+		"""
+		if kwargs:
+			self.created_at = time_conversor(kwargs["created_at"])
+			self.updated_at = time_conversor(kwargs["updated_at"])
+			for k, v in kwargs.items():
+				if k == '__class__':
+					continue
+				else:
+					setattr(self, k, v)
+		else:
+			self.id = str(uuid4())
+			self.created_at = datetime.today().isoformat()
+			self.updated_at = datetime.today().isoformat()
+			models.storage.new(self)
 
-    def __str__(self):
-        """ Define method str
-        """
-        self.__dict__.update({
-            "created_at": time_conversor(self.created_at),
-            "updated_at": time_conversor(self.updated_at),
-        })
-        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+	def __str__(self):
+		""" Define method str
+		"""
+		self.__dict__.update({
+			"created_at": time_conversor(self.created_at),
+			"updated_at": time_conversor(self.updated_at),
+		})
+		return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
-    def __repr__(self):
-        """ Define method repr
-        """
-        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+	def __repr__(self):
+		""" Define method repr
+		"""
+		return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
-    def save(self):
-        """ method save
-        """
-        self.updated_at = datetime.today()
-        models.storage.save()
+	def save(self):
+		""" method save
+		"""
+		self.updated_at = datetime.today()
+		models.storage.save()
 
-    def to_dict(self):
-        """ Methods to dictionary
-        """
-        if type(self.created_at) in [str]:
-            self.created_at = time_conversor(self.created_at)
-        if type(self.updated_at) in [str]:
-            self.updated_at = time_conversor(self.updated_at)
-        self.created_at = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        self.updated_at = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        aux_val = (self.__dict__).copy()
-        aux_val['__class__'] = self.__class__.__name__
-        return aux_val
+	def to_dict(self):
+		""" Methods to dictionary
+		"""
+		if type(self.created_at) in [str]:
+			self.created_at = time_conversor(self.created_at)
+		if type(self.updated_at) in [str]:
+			self.updated_at = time_conversor(self.updated_at)
+		self.created_at = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+		self.updated_at = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+		aux_val = (self.__dict__).copy()
+		aux_val['__class__'] = self.__class__.__name__
+		return aux_val
